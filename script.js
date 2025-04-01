5dd66c23aea753e1e88eb3b890f9cae897947737
justinebuisson606 <justine.buisson@etu.minesparis.psl.eu>
2025-04-01 16:06:11 +0200

    voilà le projet

--- script.js
+++ script.js
@@ -3,3 +3,71 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const pad = calculator.querySelector('.calculator__display')
+
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const pad = document.querySelector('.calculator__display')
+    const padContent = pad.textContent
+    const previousKeyType = calculator.dataset.previousKeyType
+
+    if (!action) {
+        calculator.dataset.previousKeyType = 'numberkey'
+        if (padContent === '0'|| previousKeyType === 'operator') {
+          pad.textContent = keyContent
+        } else {
+          pad.textContent = padContent + keyContent
+    
+        }
+      }
+    
+    
+    
+
+    if (
+    action === 'add'||action === 'subtract' || action === 'multiply'|| action === 'divide')
+    {
+        console.log('on additionne!')
+        key.classList.add('is-depressed')
+        calculator.dataset.previousKeyType = 'operator'
+        calculator.dataset.firstValue = padContent
+        calculator.dataset.operator = action
+          }
+    
+
+    const calculate = (n1, operator, n2) => {
+        let result = ''
+      
+        if (operator === 'add') {
+          result = parseInt(n1) + parseInt(n2)
+        } else if (operator === 'subtract') {
+          result = parseInt(n1) - parseInt(n2)
+        } else if (operator === 'multiply') {
+          result = parseInt(n1) * parseInt(n2)
+        } else if (operator === 'divide') {
+          result = parseInt(n1) / parseInt(n2)
+        }
+      
+        return result
+      }
+    if (action === 'calculate') {
+        const firstValue = calculator.dataset.firstValue
+        const operator = calculator.dataset.operator
+        const secondValue = padContent
+      
+        pad.textContent = calculate(firstValue, operator, secondValue)
+      }
+    
+    Array.from(key.parentNode.children).forEach(k =>
+        k.classList.remove('is-depressed'))
+  }
+})
+
